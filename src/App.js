2778abd301bb42802db8738eeb7678a4ff640617
Joe Praise <alimipraisejoe@gmail.com>
2023-10-09 22:42:20 +0100

    create movie schedule

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Counter from './components/Counter/Home/Counter';
-import CounterBooking from './components/Counter/Booking/CounterBooking';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Counter from "./components/Counter/Home/Counter";
+import CounterBooking from "./components/Counter/Booking/CounterBooking";
+import MovieScheduleBooking from "./components/Movie schedule/Movie schedule Booking/MovieScheduleBooking";
 
 function App() {
   return (
     <div className="App">
-       <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Counter />} />
-          <Route path='/booking/:id' element={<CounterBooking />} />
+          <Route path="/" element={<Counter />} />
+          <Route path="/booking/:id" element={<CounterBooking />} />
+          <Route
+            path="/movieschedulebooking"
+            element={<MovieScheduleBooking />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
