4deda3dca2bea3f1ddfd039222bd4b3b843b2048
UncleLenny <lengshakjoseph4@gmail.com>
2023-10-09 17:42:58 +0100

    theatre admin

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Counter from './components/Counter/Home/Counter';
 import CounterBooking from './components/Counter/Booking/CounterBooking';
+import TheaterHome from './components/Theater/Landing/TheaterHome';
 
 function App() {
   return (
     <div className="App">
        <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Counter />} />
+          <Route path='/counter' element={<Counter />} />
           <Route path='/booking/:id' element={<CounterBooking />} />
+          <Route path='/theaterAdmin' element={<TheaterHome />} />
         </Routes>
       </BrowserRouter>
     </div>
