c371c054fd614db03711d1232b49267e9f4459cd
UncleLenny <lengshakjoseph4@gmail.com>
2023-10-11 23:48:35 +0100

    profile page added and update to other pages

---
+++ src/components/Counter/Profile/Profile.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../stylesCounter/profile.css'
+import CounterNav from '../Navigation/CounterNav'
+import dp from '../../uploads/dpdp.jpg'
+
+function Profile() {
+  return (
+    <div className='cPPage'>
+        <CounterNav />
+        <div className='cProfile'>
+            <div className='cProfileInfo'>
+                <img src={dp} alt="" />
+                <div className='cProfileDetails'>
+                    <h3>First Name:</h3> <span>Stella</span>
+                    <h3>Last Name:</h3> <span>Lodan</span>
+                    <h3>Phone Number:</h3> <span>+234 813 220 5569</span>
+                    <h3>Address:</h3> <span>24 I.T. Igbani Street, Jabi 900108, Abuja</span>
+                </div>
+            </div>
+            <div className='cUserandPass'>
+                <h4>Username:</h4> <span>Counter001SL</span>
+                <button>Change Password</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Profile
--- src/components/Theater/View Tables/ViewSeatingLayer.js
+++ src/components/Theater/View Tables/ViewSeatingLayer.js
@@ -17,78 +17,77 @@ function ViewSeatingLayer() {
   return (
     <div className="vs-layer">
       <TheaterNav />
-      <div className="booking-main">
-        <div className="booking-container">
-          <div className="booking-container-seat">
-            <p className="booking-choose-seat-text">Seats Available</p>
-            <div className="booking-seat-side">
-              <span className="booking-seat-vvip"></span>
+      <div className="vs-main">
+        <div className="vs-container">
+          <div className="vs-container-seat">
+            <p className="vs-choose-seat-text">Seats Available</p>
+            <div className="vs-seat-side">
+              <span className="vs-seat-vvip"></span>
               <p>VVIP</p>
             </div>
-            <div className="booking-seat-side">
-              <span className="booking-seat-vip"></span>
+            <div className="vs-seat-side">
+              <span className="vs-seat-vip"></span>
               <p>VIP</p>
             </div>
-            <div className="booking-seat-side">
-              <span className="booking-seat-regular"></span>
+            <div className="vs-seat-side">
+              <span className="vs-seat-regular"></span>
               <p>Regular</p>
             </div>
             <div>
-                <div className="booking-total">
-                    <h3 className="booking-totalh">Theater Capacity</h3>
-                    :
-                    <p className="booking-totalp">50</p>
-                </div>
+              <div className="vs-total">
+                <h3 className="vs-totalh">Theater Capacity</h3>:
+                <p className="vs-totalp">50</p>
+              </div>
             </div>
           </div>
-          <div className="booking-container-col1">
-            <div className="seat-col-flex"> 
-                <div className="box-line">
-                <p className="line"></p>
-                </div>
-                <div className="box-container">
-                <div className="main-boxx">
-                    <div className="box1">
+          <div className="vs-container-col1">
+            <div className="vs-col-flex">
+              <div className="vs-box-line">
+                <p className="vs-line"></p>
+              </div>
+              <div className="vs-box-container">
+                <div className="vs-main-boxx">
+                  <div className="vs-box1">
                     {col_matrix_1.map((r, i1) => {
-                        let arr = new Array(r).fill(3);
+                      let arr = new Array(r).fill(3);
 
-                        return (
-                        <div className="box1-col1" key={i1}>
-                            {arr.map((c, i2) => (
+                      return (
+                        <div className="vs-box1-col1" key={i1}>
+                          {arr.map((c, i2) => (
                             <p
-                                className="col1p"
-                                key={i2}
-                                onClick={(e) => setActive(e)}
+                              className="vs-col1p"
+                              key={i2}
+                              onClick={(e) => setActive(e)}
                             >
-                                <span></span>
+                              <span></span>
                             </p>
-                            ))}
+                          ))}
                         </div>
-                        );
+                      );
                     })}
-                    </div>
-                    <div className="box2">
+                  </div>
+                  <div className="vs-box2">
                     {col_matrix_2.map((r, i1) => {
-                        let arr = new Array(r).fill(3);
+                      let arr = new Array(r).fill(3);
 
-                        return (
-                        <div className="box1-col1" key={i1}>
-                            {arr.map((c, i2) => (
+                      return (
+                        <div className="vs-box1-col1" key={i1}>
+                          {arr.map((c, i2) => (
                             <p
-                                className="col1p"
-                                key={i2}
-                                onClick={(e) => setActive(e)}
+                              className="vs-col1p"
+                              key={i2}
+                              onClick={(e) => setActive(e)}
                             >
-                                <span></span>
-                                <i className="regular"></i>
+                              <span></span>
+                              <i className="vs-regular"></i>
                             </p>
-                            ))}
+                          ))}
                         </div>
-                        );
+                      );
                     })}
-                    </div>
+                  </div>
                 </div>
-                </div>
+              </div>
             </div>
           </div>
         </div>
