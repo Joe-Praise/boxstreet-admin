baaab5fd61915d818442ee1842fae4144ebc8576
UncleLenny <lengshakjoseph4@gmail.com>
2023-10-18 13:24:36 +0100

    reduce the code in login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -90,17 +90,16 @@ function SignInForm({
         setLoading(false)
         let info = response.data?.data
        
+        localStorage.setItem('branch_id',info.user.branch_id);
+        localStorage.setItem('cinema_id',info.user.cinema_id);
+        localStorage.setItem('user_id',info.user._id);
+
         if(info.user.role === "COUNTER"){
             navigate("/counter");
-            localStorage.setItem('branch_id',info.user.branch_id);
-            localStorage.setItem('cinema_id',info.user.cinema_id);
-            localStorage.setItem('user_id',info.user._id);
         }
+
         if(info.user.role === "THEATER"){
             navigate("/theater");
-            localStorage.setItem('branch_id',info.user.branch_id);
-            localStorage.setItem('cinema_id',info.user.cinema_id);
-            localStorage.setItem('user_id',info.user._id);
         }
 
       } else {
